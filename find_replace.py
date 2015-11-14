bf21d85a85f5d3e3d1b877552a1d745096da97d8
mtoreihi <mtoreihi@gmail.com>
2015-11-14 17:13:42 +0330

    Adding file and directory names substitution support

--- find_replace.py
+++ find_replace.py
@@ -7,7 +7,7 @@ import subprocess
 import os
 
 # Define path
-mypath = "/opt/mtoreihi/test"
+mypath = "/mtoreihi/path"
 myfile = "words.txt"
 
 
@@ -19,7 +19,12 @@ except:
 
 # Loop through every line in file
 for line in fp:
-	wf,wr = line.split('=')
+	try:
+		wf,wr = line.split('=')
+	except:
+		print "File format is wrong. It should be word_find=word_replace per line."
+		fp.close()
+		exit()
 	wf = wf.rstrip()
 	wr = wr.rstrip()
 	# check for bad file format
@@ -27,6 +32,7 @@ for line in fp:
 		print "File format is wrong. It should be word_find=word_replace per line."
 		fp.close()
 		exit()
+	# loop through file content
 	for p,d,f in os.walk(mypath):
 		for files in f:
 			file_path = p + '/' + files
@@ -34,5 +40,21 @@ for line in fp:
 			substitute = 's/' + wf + '/' + wr + '/g'
 			subprocess.Popen(['sed', '-i', substitute, file_path])
 	
-
+	# loop through file & directory names
+	for p,d,f in os.walk(mypath):
+		for dirs in d:
+			if (dirs.find(wf) != -1):
+				# replace dir names
+                newname = dirs.replace(wf,wr)
+                oldpath = p + "/" + dirs
+                newpath = p + "/" + newname
+				substitute = "mv " + oldpath + " " + newpath
+				os.system(substitute)
+		for files in f:
+			if (files.find(wf) != -1): 
+                newname = files.replace(wf,wr)
+                oldpath = p + "/" + files
+                newpath = p + "/" + newname
+				substitute = "mv " + oldpath + " "  + newpath
+				os.system(substitute)
 fp.close()
